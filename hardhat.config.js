8bc4ac37eaeb3f1c1a9e7b9426406802bfa1bd3d
Anton Grigorev <anton.grigorev.dev@gmail.com>
2023-12-29 12:15:49 +0400

    New config

--- hardhat.config.js
+++ hardhat.config.js
@@ -20,16 +20,14 @@ module.exports = {
             url: `https://earpc.apothem.network`,
             accounts: [fs.readFileSync("./privateKey").toString()],
         },
+        xdc: {
+            url: `https://erpc.xdcrpc.com`,
+            accounts: [fs.readFileSync("./privateKey").toString()],
+        },
         ganache: {
             url: `http://127.0.0.1:8545`,
             accounts: [fs.readFileSync("./privateKey").toString()],
         },
-        // hardhat: {
-        //     forking: {
-        //         url: "http://127.0.0.1:8545",
-        //         accounts: [fs.readFileSync("./privateKey").toString()],
-        //     },
-        // },
     },
     namedAccounts: {
         deployer: 0,
